1dcfd71e572721b17a4d43a07f196f6c97a896b6
emilio.brazil <emilio.brazil@bbf0f74a-103a-11de-a4b8-0f3aab1a9b0e>
2009-03-26 23:43:22 +0000

    curve fitting

---
+++ C++/curveFitting/curveFiting/drawarea.h
@@ -0,0 +1,12 @@
+#ifndef DRAWAREA_H
+#define DRAWAREA_H
+
+#include <QWidget>
+
+class DrawArea : public QWidget
+{
+public:
+    DrawArea(QWidget *parent = 0);
+};
+
+#endif // DRAWAREA_H
--- C++/curveFitting/curveFiting/lineScan.h
+++ C++/curveFitting/curveFiting/lineScan.h
@@ -1,11 +1,13 @@
 #ifndef LINESCAN_H
 #define LINESCAN_H
 
-#include <QPainter>
+#include <primitive_types.h>
 
-void dda( QPainter &painter , QPointF a , QPointF b );
+#include <QImage>
 
-void dda( QPainter &painter , QPointF a , QPointF b , uint32 radius );
+void dda( QImage &image , QPointF a , QPointF b );
+
+void dda( QImage &image , QPointF a , QPointF b , uint32 radius );
 
 
 #endif // LINESCAN_H
