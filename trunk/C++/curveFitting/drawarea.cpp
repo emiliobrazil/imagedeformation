073a5e65f4ee731366b9a7ae601832a3659cf42a
emilio.brazil <emilio.brazil@bbf0f74a-103a-11de-a4b8-0f3aab1a9b0e>
2009-03-28 00:26:37 +0000

    começando a implementação da estrutura df

--- trunk/C++/curveFitting/drawarea.cpp
+++ trunk/C++/curveFitting/drawarea.cpp
@@ -8,7 +8,7 @@ DrawArea::DrawArea(QWidget *parent)
     :QWidget(parent)
 {
     this->_image = QImage( 1024, 780 ,QImage::Format_ARGB32 );
-
+    this->setFocusPolicy(Qt::ClickFocus);
 }
 
 void DrawArea::mousePressEvent(QMouseEvent *event)
@@ -25,8 +25,9 @@ void DrawArea::mouseMoveEvent(QMouseEvent *event)
 {
     if(  event->buttons() == Qt::LeftButton )
     {
-       dda( this->_image , this->_lastPoint , event->pos() , 20 );
-       this->_lastPoint = event->pos();
+        pBox( this->_image , this->_lastPoint , 20  );
+        dda( this->_image , this->_lastPoint , event->pos() , 20 );
+        this->_lastPoint = event->pos();
     }
     update();
 }
@@ -45,5 +46,18 @@ void DrawArea::resizeEvent(QResizeEvent *event)
 
 void DrawArea::keyPressEvent ( QKeyEvent * event )
 {
-    // todo
+    QString fileName;
+    std::pair< uint32,QPointF > pairTeste ;
+    switch ( event->key() )
+    {
+    case Qt::Key_Q:
+        qApp->quit();
+        break;
+    case Qt::Key_C:
+        this->_image = QImage( 1024, 780 ,QImage::Format_ARGB32 );
+        break;
+    default:
+        QWidget::keyPressEvent ( event );
+    }
+    update();
 }
