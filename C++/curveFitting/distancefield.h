ed7ddc7dde84c7829cfcaaa3a61543a0a01c2436
emilio.brazil <emilio.brazil@bbf0f74a-103a-11de-a4b8-0f3aab1a9b0e>
2009-03-29 15:05:30 +0000

    avancando

--- C++/curveFitting/distancefield.h
+++ C++/curveFitting/distancefield.h
@@ -2,24 +2,22 @@
 #define DISTANCEFIELD_H
 
 #include <QPoint>
-
+#include <QImage>
 #include <vector>
 
 #include <primitive_types.h>
+#include "primitive_const.h"
 
 class DistanceField
 {
 public:
     DistanceField( uint32 w , uint32 h , uint32 radius );
-    ~DistanceField( void );
+    ~DistanceField( void ){}
 
-    real dx( uint32 i , uint32 j ) ;
-    real dy( uint32 i , uint32 j ) ;
+    inline real dx( int i , int j ){ return this->_dx[ this->_index( i , j ) ];}
+    inline real dy( int i , int j ){ return this->_dx[ this->_index( i , j ) ];}
     real dx( real x , real y ) ;
     real dy( real x , real y ) ;
-
-    void setDx( uint32 i , uint32 j ) ;
-    void setDy( uint32 i , uint32 j ) ;
 
     void putLine( QPointF a , QPointF b ) ;
     void putPoint( QPointF a ) ;
@@ -29,12 +27,29 @@ public:
     QImage toImageD( void );
     QImage toImageRGB( void );
 
-    void clear( void );
+    inline void clear( void ){
+        this->_dx.clear();
+        this->_dy.clear();
+        this->_dx = std::vector<real> ( _numberOfElements , INF) ;
+        this->_dy = std::vector<real> ( _numberOfElements , INF) ;
+    }
+
+protected:
+    inline void _setDx( int i , int j , real v ){ this->_dx[ this->_index( i , j ) ] = v ; }
+    inline void _setDy( int i , int j , real v ){ this->_dy[ this->_index( i , j ) ] = v ; }
+    inline uint32 _index( int i , int j ){ return ( i + this->_radius )*( this->_w + 2 * this->_radius ) + ( j + this->_radius );}
+
+    inline real _kBilinear( real x ){
+        if (x<-1.0f) return 0.0f;
+        else if (x<0.0f) return 1.0f+x;
+        else if  (x<1.0f) return 1.0f-x;
+        return 0.0f;
+    }
 
 private:
+    uint32 _w , _h , _radius , _numberOfElements;
     std::vector<real> _dx;
     std::vector<real> _dy;
-
 };
 
 #endif // DISTANCEFIELD_H
